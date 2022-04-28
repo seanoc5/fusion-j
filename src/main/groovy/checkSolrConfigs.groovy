289b529e78b0e52a817ac407831721baf238044e
seanoc5 <seanoc5@gmail.com>
2022-04-27 21:58:06 -0400

    add paths rather than files

--- src/main/groovy/checkSolrConfigs.groovy
+++ src/main/groovy/checkSolrConfigs.groovy
@@ -3,6 +3,10 @@ import groovy.cli.picocli.CliBuilder
 import groovy.cli.picocli.OptionAccessor
 import org.apache.log4j.Logger
 
+import java.net.http.HttpResponse
+import java.nio.file.Path
+import java.nio.file.Paths
+
 Logger log = Logger.getLogger(this.class.name);
 
 log.info "Starting ${this.class.name}..."
@@ -32,6 +36,10 @@ if (options.help) {
 }
 
 FusionClient fusionClient = new FusionClient(options)
+Path tempPath = Paths.get("tempfile.zip")
+log.info "using temp file: ${tempPath.toAbsolutePath()}"
+HttpResponse.BodyHandler bodyHandler = HttpResponse.BodyHandlers.ofFile(tempPath)
+def objects = fusionClient.getObjects("", bodyHandler)
 def configSets = fusionClient.getConfigSets()
 
 log.info "ConfigSets: $configSets"
