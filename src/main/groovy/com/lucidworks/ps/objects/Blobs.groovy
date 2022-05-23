1392849aefed0e532df619cba1211bbd6d033dcf
Sean O'Connor <seanoc5@gmail.com>
2022-05-23 14:42:34 -0400

    encapsulating logic

---
+++ src/main/groovy/com/lucidworks/ps/objects/Blobs.groovy
@@ -0,0 +1,16 @@
+package com.lucidworks.ps.objects
+
+/**
+ * @author :    sean
+ * @mailto :    seanoc5@gmail.com
+ * @created :   5/18/22, Wednesday
+ * @description:
+ */
+
+/**
+ * class to gather blobs from a fusion client or from the filesystem
+ */
+class Blobs {
+    List blobExports = []
+
+}
