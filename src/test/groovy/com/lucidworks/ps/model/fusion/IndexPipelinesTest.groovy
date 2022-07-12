3d12ef1f1b9072fb413540918b17c26bac591c73
SeanO'Connor <sean.oconnor@lucidworks.com>
2022-07-12 19:51:20 -0400

    add code to test parsing (and evalauting) IndexPipelines

---
+++ src/test/groovy/com/lucidworks/ps/model/fusion/IndexPipelinesTest.groovy
@@ -0,0 +1,13 @@
+package com.lucidworks.ps.model.fusion
+
+import spock.lang.Specification
+
+/**
+ * test parsing and evaluating index pipelines
+ */
+class IndexPipelinesTest extends Specification {
+
+    def "should load an example indexpipeline from file - basic test"(){
+
+    }
+}
