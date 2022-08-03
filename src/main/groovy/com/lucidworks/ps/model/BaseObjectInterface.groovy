268e03a2b5e5345f6c249805aa60d76030708be3
Sean O'Connor <seanoc5@gmail.com>
2022-08-03 18:57:59 -0400

    remove/comment out interface -- delete it?

--- src/main/groovy/com/lucidworks/ps/model/BaseObjectInterface.groovy
+++ src/main/groovy/com/lucidworks/ps/model/BaseObjectInterface.groovy
@@ -1,29 +1,32 @@
-package com.lucidworks.ps.model
+//package com.lucidworks.ps.model
 
-import com.lucidworks.ps.clients.FusionClient
-import groovy.json.JsonGenerator
-import org.apache.log4j.Logger
-/**
- * Base object to facilitate exporting to various destinations (filesystem, fusion instance,...)
- */
-public interface BaseObjectInterface {
-    static final Logger log = Logger.getLogger(this.class.name);
-    static def jsonDefaultOutput = new JsonGenerator.Options()
-            .dateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
-            .build()
+//import com.lucidworks.ps.clients.FusionClient
+//import groovy.json.JsonGenerator
+//import org.apache.log4j.Logger
 
-    List<Map> jsonItems = []
+// removing interface, and sticking with BaseObject as abstract class
 
-    default def export() {
-        // todo add default functionality here...?
-        log.info "export me"
-    }
-
-    def export(File exportFolder)
-
-    def export(FusionClient fusionClient)
-
-    default int size(){
-        return jsonItems.size()
-    }
-}
+///**
+// * Base object to facilitate exporting to various destinations (filesystem, fusion instance,...)
+// */
+//public interface BaseObjectInterface {
+//    static final Logger log = Logger.getLogger(this.class.name);
+//    static def jsonDefaultOutput = new JsonGenerator.Options()
+//            .dateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+//            .build()
+//
+//    List<Map> jsonItems = []
+//
+//    default def export() {
+//        // todo add default functionality here...?
+//        log.info "export me"
+//    }
+//
+//    def export(File exportFolder)
+//
+//    def export(FusionClient fusionClient)
+//
+//    default int size(){
+//        return jsonItems.size()
+//    }
+//}
