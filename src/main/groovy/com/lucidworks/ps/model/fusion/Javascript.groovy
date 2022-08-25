0f2a2c3da08f7ef14605d3ef81da0c02237029f1
SeanO'Connor <sean.oconnor@lucidworks.com>
2022-08-25 15:14:34 -0400

    improve stage grouping and tagging (lines as well)

--- src/main/groovy/com/lucidworks/ps/model/fusion/Javascript.groovy
+++ src/main/groovy/com/lucidworks/ps/model/fusion/Javascript.groovy
@@ -44,10 +44,10 @@ class Javascript extends BaseObject {
             String lineTrimmed = line.trim()
             if (lineTrimmed) {
                 if (lineTrimmed.contains('//')) {
-                    List<String> parts = lineTrimmed.split(/\/\//)
+                    List<String> parts = lineTrimmed.split(/\/\//, 2)
                     log.debug "Stripping comment from line:[$lineTrimmed] -> [${parts[0]}]"
-                    lineTrimmed = parts[0]
-                    String comment = parts[1]
+                    lineTrimmed = parts[0].trim()
+                    String comment = '//'+parts[1]
                     groupedLines.comments << comment
                 } else {
                     log.debug "regular "
@@ -55,28 +55,63 @@ class Javascript extends BaseObject {
 
 
                 switch (lineTrimmed) {
+                    case '':
+                        type= 'blank'
+                        break
+
                     case ~/(var|let) .* *= *\w+\..*/:
                         type = 'java package import'
                         break
                     case ~/(var|let) .*=.* new (.*)/:
                         type = 'java variable init'
                         break
+                    case ~/(Packages|java)\..*/:
+                        type = 'java library call?'
+                        break
+
                     case ~/(var|let).*=.*/:
                         type = 'variable init'
                         break
-                    case ~/(\w+) *=.*\w+.*/:
+                    case ~/(.*= *)?\w+\.\w+\(.*\).*/:           // overlap with control statements,but we are most concerned with function calls for evaluation
+                        type = 'method call'
+                        break
+                    case ~/(\w+) *[+-]?=.*\w+.*/:
                         type = 'variable assignment'
                         break
-                    case ~/function *\(\w+\) *\{/:
+                    case ~/function *(\w+)?\(.*\) *\{/:
                         type = 'function declaration'
                         break
-                    case ~/(if *\(.*\)|} else .*) *\{/:
+                    case ~/(if *\(.*\)|[} ]*else\b[{ (]*).*/:
                         type = 'if-conditional'
                         break
+
                     case ~/(console|log).*/:
                         type = 'logging'
                         break
-                    case ~/(})/:
+
+                    case ~/try\b *\{?/:
+                        type = 'try block '
+                        break
+                    case ~/[} ]*catch\b.*/:
+                        type='catch block'
+                        break
+                    case ~/.*\bthrow\b.*/:
+                        type='throw block'
+                        break
+
+                    case ~/while\b.*/:
+                        type='while block'
+                        break
+
+                    case ~/for\b.*/:
+                        type='for block'
+                        break
+
+                    case ~/[)(}{]+/:
+                        type = 'syntax'
+                        break
+
+                    case ~/(return\b.*|doc;?|document;?)/:
                         type = 'syntax'
                         break
 
@@ -94,7 +129,7 @@ class Javascript extends BaseObject {
             it.key + ":" + it.value.size()
         }
 
-        return grouped
+        return groupedLines
     }
 
 
@@ -116,8 +151,12 @@ class Javascript extends BaseObject {
     SolrInputDocument toSolrInputDocument() {
         SolrInputDocument sid = new SolrInputDocument()
         sid.setField('id', this.label)
-        sid.addField('script_t', script)
+        sid.addField('label_s', label)
+        sid.addField('type_s', itemType)
+//        sid.addField('_lw_data_source_s', 'javascript stage extrac')
+        sid.addField('script_txt', script)
         sid.addField('sourceType_s', sourceType)
+        sid.addField('jslines_txt', lines)
 
         if (!groupedLines) {
             groupedLines = groupLines()
@@ -125,8 +164,10 @@ class Javascript extends BaseObject {
         }
         groupedLines.each { String group, List<String> lines ->
             log.debug "\t\t add groupded lines ($group) with ${lines.size()} lines"
-            sid.addField("${group}_t", lines)
+            sid.addField("jsTag-${group}_txt", lines)
         }
+        sid.addField('tag_ss', groupedLines.keySet())
+        return sid
     }
 
 }
