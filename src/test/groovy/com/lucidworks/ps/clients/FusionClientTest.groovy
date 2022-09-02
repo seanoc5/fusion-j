b1ad30443074d4fc902c99ccab50a80dff44fc97
SeanO'Connor <sean.oconnor@lucidworks.com>
2022-09-02 18:42:21 -0400

    add test for body handler (zip file or stream)

--- src/test/groovy/com/lucidworks/ps/clients/FusionClientTest.groovy
+++ src/test/groovy/com/lucidworks/ps/clients/FusionClientTest.groovy
@@ -1,14 +1,19 @@
 package com.lucidworks.ps.clients
 
+import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream
 import spock.lang.Specification
 
 import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import java.nio.file.Path
+import java.nio.file.Paths
+
 /**
  * Testing fusion client
  * Note: this should be a functional test, not a unit test, refactor as appropriate...
  */
 class FusionClientTest extends Specification {
-    String appName= 'test'
+    String appName = 'test'
     String furl = 'http://newmac:8764'
     FusionClient client = new FusionClient(furl, 'sean', 'pass1234', appName)
 
@@ -20,7 +25,7 @@ class FusionClientTest extends Specification {
 
         then:
         apiInfo instanceof Map
-        client.majorVersion >=4
+        client.majorVersion >= 4
     }
 
     def "test buildGetRequest"() {
@@ -82,16 +87,68 @@ class FusionClientTest extends Specification {
         String q = '*:*'
         String collName = appName
         String qrypName = appName
-        Map qryParams = [q:q]
+        Map qryParams = [q: q]
 
         when:
-        FusionResponseWrapper fwr = client.query(appName, collName, qrypName, qryParams, )
+        FusionResponseWrapper fwr = client.query(appName, collName, qrypName, qryParams,)
         Map info = fwr.parsedInfo
         Map response = info.response
 
         then:
         fwr.wasSuccess()
         response.keySet().contains('numFound')
+
+    }
+
+    def "get configset map response- test"() {
+        given:
+        List<String> configsetsToGet = ['upval', 'Keymatches']
+
+        when:
+        def results = client.getConfigSets(configsetsToGet)
+//        FusionResponseWrapper fwr = client.getConfigSets()
+//        Map info = fwr.parsedInfo
+//        Map response = info.response
+
+        then:
+        results instanceof Map
+//        fwr.wasSuccess()
+//        response.keySet().contains('numFound')
+
+    }
+
+    def "export objects ofFile response - collection test"() {
+        given:
+        List<String> collectionToGet = ['upval', 'Keymatches']
+        String exportParams= 'collection.ids=test'
+        Path exportFolder = Paths.get(getClass().getResource('/').toURI())
+        Path exportZip = Paths.get(exportFolder.toAbsolutePath().toString(), 'collection.test.zip')
+
+        when:
+        def results = client.exportFusionObjects(exportParams, exportZip)
+
+        then:
+        results instanceof Map
+
+    }
+    def "export objects inputstream response - collection test"() {
+        given:
+        List<String> collectionToGet = ['upval', 'Keymatches']
+        String exportParams= 'collection.ids=test'
+        Path exportFolder = Paths.get(getClass().getResource('/').toURI())
+        Path exportZip = Paths.get(exportFolder.toAbsolutePath().toString(), 'collection.test.zip')
+
+        when:
+        HttpResponse results = client.exportFusionObjects(exportParams, exportZip)
+        println("Results class type: ${results.getClass().name}")
+//        InputStream bis = results.body()
+        ZipArchiveInputStream zais = results.body()
+
+        then:
+        results instanceof HttpResponse
+        zais.canReadEntryData()
+        zais.compressedCount > 0
+//        results instanceof Map
 
     }
 }
